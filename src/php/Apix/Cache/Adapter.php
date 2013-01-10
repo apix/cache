0f087c3a5a95f5194e044335b5ad03870015864f
Franck Cassedanne <franck@ouarz.net>
2013-01-10 17:46:35 +0000

    added (c) comments

--- src/php/Apix/Cache/Adapter.php
+++ src/php/Apix/Cache/Adapter.php
@@ -1,4 +1,15 @@
 <?php
+
+/**
+ *
+ * This file is part of the Apix Project.
+ *
+ * (c) Franck Cassedanne <franck at ouarz.net>
+ *
+ * @license     http://opensource.org/licenses/BSD-3-Clause  New BSD License
+ *
+ */
+
 namespace Apix\Cache;
 
 interface Adapter
