8de989b7e35441128181405932bfad4eae9eb90e
Franck Cassedanne <franck@ouarz.net>
2013-01-30 10:36:06 +0000

    Inherits GenericTestCase

--- src/tests/Apix/Cache/MongoTest.php
+++ src/tests/Apix/Cache/MongoTest.php
@@ -12,9 +12,7 @@
 
 namespace Apix\Cache;
 
-use Apix\TestCase;
-
-class MongoTest extends TestCase
+class MongoTest extends GenericTestCase
 {
     protected $cache, $mongo;
 
@@ -40,14 +38,9 @@ class MongoTest extends TestCase
     {
         if (null !== $this->cache) {
             $this->cache->flush();
-            unset($this->cache);
             $this->mongo->close();
+            unset($this->cache, $this->mongo);
         }
-    }
-
-    public function testLoadReturnsNullWhenEmpty()
-    {
-        $this->assertNull($this->cache->load('id'));
     }
 
     public function testSaveIsUnique()
@@ -55,102 +48,16 @@ class MongoTest extends TestCase
         $this->assertTrue($this->cache->save('bar1', 'foo'));
         $this->assertTrue($this->cache->save('bar2', 'foo'));
 
-        $this->assertEquals('bar2', $this->cache->load('foo'));
+        $this->assertEquals('bar2', $this->cache->loadKey('foo'));
 
         $this->assertEquals(1, $this->cache->count('foo') );
     }
 
-    public function testSaveAndLoadWithString()
-    {
-        $this->assertTrue($this->cache->save('strData', 'id'));
-
-        $this->assertEquals('strData', $this->cache->load('id'));
-    }
-
-    public function testSaveAndLoadWithArray()
-    {
-        $data = array('foo' => 'bar');
-        $this->assertTrue($this->cache->save($data, 'id'));
-        $this->assertEquals($data, $this->cache->load('id'));
-    }
-
-    public function testSaveAndLoadWithObject()
-    {
-        $data = new \stdClass;
-        $this->assertTrue($this->cache->save($data, 'id'));
-        $this->assertEquals($data, $this->cache->load('id'));
-    }
-
-    public function testSaveAndLoadArray()
-    {
-        $data = array('arrayData');
-        $this->assertTrue($this->cache->save($data, 'id'));
-
-        $this->assertEquals($data, $this->cache->load('id'));
-    }
-
-    public function testSaveWithTags()
-    {
-        $this->assertTrue(
-            $this->cache->save('strData1', 'id1', array('tag1', 'tag2'))
-        );
-
-        $this->assertTrue(
-            $this->cache->save('strData2', 'id2', array('tag3', 'tag4'))
-        );
-
-        $ids = $this->cache->load('tag2', 'tag');
-
-        $this->assertEquals( array($this->cache->mapKey('id1')), $ids );
-    }
-
-    public function testSaveWithTagDisabled()
-    {
-        $this->cache->setOptions(array('tag_enable' => false));
-
-        $this->assertTrue(
-            $this->cache->save('strData1', 'id1', array('tag1', 'tag2'))
-        );
-
-        $this->assertNull($this->cache->load('tag1', 'tag'));
-    }
-
-    public function testSaveWithOverlappingTags()
-    {
-        $this->assertTrue(
-            $this->cache->save('strData1', 'id1', array('tag1', 'tag2'))
-        );
-
-        $this->assertTrue(
-            $this->cache->save('strData2', 'id2', array('tag2', 'tag3'))
-        );
-
-        $ids = $this->cache->load('tag2', 'tag');
-        $this->assertTrue(count($ids) == 2);
-        $this->assertContains($this->cache->mapKey('id1'), $ids);
-        $this->assertContains($this->cache->mapKey('id2'), $ids);
-    }
-
-    public function testClean()
-    {
-        $this->cache->save('strData1', 'id1', array('tag1', 'tag2'));
-        $this->cache->save('strData2', 'id2', array('tag2', 'tag3', 'tag4'));
-        $this->cache->save('strData3', 'id3', array('tag3', 'tag4'));
-
-        $this->assertTrue($this->cache->clean(array('tag4')));
-        $this->assertFalse($this->cache->clean(array('tag4')));
-
-        $this->assertNull($this->cache->load('id2'));
-        $this->assertNull($this->cache->load('id3'));
-        $this->assertNull($this->cache->load('tag4', 'tag'));
-        $this->assertEquals('strData1', $this->cache->load('id1'));
-    }
-
     public function testFlushCacheOnly()
     {
-        $this->cache->save('strData1', 'id1', array('tag1', 'tag2'));
-        $this->cache->save('strData2', 'id2', array('tag2', 'tag3'));
-        $this->cache->save('strData3', 'id3', array('tag3', 'tag4'));
+        $this->cache->save('data1', 'id1', array('tag1', 'tag2'));
+        $this->cache->save('data2', 'id2', array('tag2', 'tag3'));
+        $this->cache->save('data3', 'id3', array('tag3', 'tag4'));
 
         $foo = array('foo' => 'bar');
         $this->cache->collection->insert($foo);
@@ -158,49 +65,26 @@ class MongoTest extends TestCase
         $this->assertTrue($this->cache->flush());
 
         $this->assertEquals(
-            $foo,
-            $this->cache->collection->findOne(array('foo'=>'bar'))
+            $foo, $this->cache->collection->findOne(array('foo'=>'bar'))
         );
 
-        $this->assertNull($this->cache->load('id3'));
-        $this->assertNull($this->cache->load('tag1', 'tag'));
+        $this->assertNull($this->cache->loadKey('id3'));
+        $this->assertNull($this->cache->loadTag('tag1'));
     }
 
     public function testFlushAll()
     {
-        $this->cache->save('strData1', 'id1', array('tag1', 'tag2'));
-        $this->cache->save('strData2', 'id2', array('tag2', 'tag3'));
-        $this->cache->save('strData3', 'id3', array('tag3', 'tag4'));
+        $this->cache->save('data1', 'id1', array('tag1', 'tag2'));
+        $this->cache->save('data2', 'id2', array('tag2', 'tag3'));
+        $this->cache->save('data3', 'id3', array('tag3', 'tag4'));
 
         $this->cache->collection->insert(array('key' => 'foobar'));
 
         $this->assertTrue($this->cache->flush(true));
         $this->assertNull($this->cache->collection->findOne(array('key')));
 
-        $this->assertNull($this->cache->load('id3'));
-        $this->assertNull($this->cache->load('tag1', 'tag'));
-    }
-
-    public function testDelete()
-    {
-        $this->cache->save('strData1', 'id1', array('tag1', 'tag2', 'tagz'));
-        $this->cache->save('strData2', 'id2', array('tag2', 'tag3'));
-
-        $this->assertTrue($this->cache->delete('id1'));
-
-        $this->assertNull($this->cache->load('id1'), 'msg1');
-        $this->assertNull($this->cache->load('tag1', 'tag'), 'msg2');
-        $this->assertNull($this->cache->load('tagz', 'tag'), 'msg3');
-
-        $this->assertContains(
-            $this->cache->mapKey('id2'),
-            $this->cache->load('tag2', 'tag')
-        );
-    }
-
-    public function testDeleteInexistant()
-    {
-        $this->assertFalse($this->cache->delete('Inexistant'));
+        $this->assertNull($this->cache->loadKey('id3'));
+        $this->assertNull($this->cache->loadTag('tag1'));
     }
 
     public function testShortTtlDoesExpunge()
@@ -214,7 +98,7 @@ class MongoTest extends TestCase
         //     'reIndex' => 'cache'
         // ));
 
-        $this->assertNull( $this->cache->load('ttlId') );
+        $this->assertNull( $this->cache->loadKey('ttlId') );
     }
 
 }
