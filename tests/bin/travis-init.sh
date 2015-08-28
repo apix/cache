5a719812b863a85434300fa9315f40b6ecd1b362
Franck Cassedanne <franck@ouarz.net>
2015-08-28 16:04:17 +0100

    Refactoring Travis scripts (6)

--- tests/bin/travis-init.sh
+++ tests/bin/travis-init.sh
@@ -10,7 +10,6 @@ else
     PHPINI=~/.phpenv/versions/$VERSION/etc/php.ini
 fi
 
-# Install APC/APCu
 if [ "$DB" = "apc" ]; then
     if  [ "${VERSION}" = "hhvm" ] || [ "$(expr "${VERSION}" "<" "5.5")" -eq 1 ]
     then
@@ -21,5 +20,26 @@ if [ "$DB" = "apc" ]; then
     echo "apc.enable_cli = 1" >> $PHPINI
 fi
 
-composer self-update
-composer install --dev --prefer-source --no-interaction 
+if [ "$DB" = "redis" ]; then
+    git clone --branch=master --depth=1 git://github.com/nicolasff/phpredis.git phpredis
+    cd phpredis && phpize && ./configure && make && sudo make install && cd ..
+    rm -fr phpredis
+    echo "extension = redis.so" >> $PHPINI
+fi
+
+if [ "$DB" = "mongodb" ]; then
+    echo "extension = mongo.so" >> $PHPINI
+fi
+
+if [ "$DB" = "mysql" ]; then
+    mysql -e 'CREATE DATABASE IF NOT EXISTS apix_tests;'
+fi
+
+if [ "$DB" = "pgsql" ]; then
+    psql -c 'DROP DATABASE IF EXISTS apix_tests;' -U postgres
+    psql -c 'CREATE DATABASE apix_tests;' -U postgres
+fi
+
+if [ "$DB" = "memcached" ]; then
+    echo "extension = memcached.so" >> $PHPINI
+fi
