ca2020feba6512bac2d340eada10ea2dde60eea0
Franck Cassedanne <franck@ouarz.net>
2015-08-28 15:13:01 +0100

    Refactoring Travis scripts (3)

--- tests/bin/travis-init.sh
+++ tests/bin/travis-init.sh
@@ -4,21 +4,16 @@ set -o pipefail
 
 PHP_INI_FILE=$(php -r 'echo php_ini_loaded_file();')
 
-if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
-
-    if [ "$DB" = "apc" ]; then
-        if [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
-            echo "install APC"
-            echo "extension = apc.so" >> $PHP_INI_FILE
-        else
-            echo "install APCu-beta"
-            echo "yes" | pecl install apcu-beta
-            echo "extension = apcu.so" >> $PHP_INI_FILE
-        fi
-        echo "apc.enable_cli = 1" >> $PHP_INI_FILE
+# Install APC/APCu
+if [ "$DB" = "apc" ]; then
+    if  [ "$TRAVIS_PHP_VERSION" = "hhvm" ] || [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
+        echo "extension = apc.so" >> $PHP_INI_FILE
+    else
+        echo "yes" | pecl install apcu-beta
+        echo "extension = apcu.so" >> $PHP_INI_FILE
     fi
-
 fi
+echo "apc.enable_cli = 1" >> $PHP_INI_FILE
 
 composer self-update
 composer install --dev --prefer-source --no-interaction 
