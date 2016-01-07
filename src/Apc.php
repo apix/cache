62989cf19405280e09ed702d4e0ab710e591613b
Franck Cassedanne <franck@ouarz.net>
2016-01-07 20:15:06 +0000

    Fix HHVM

--- src/Apc.php
+++ src/Apc.php
@@ -202,7 +202,8 @@ class Apc extends AbstractCache
     public function getInternalInfos($key)
     {
         $iterator = $this->getIterator(
-            '/^' . preg_quote($this->options['prefix_key']) . '/'
+            '/^' . preg_quote($this->options['prefix_key']) . '/', 
+            \APC_ITER_KEY // fix HHVM
         );
 
         $key = $this->mapKey($key);
